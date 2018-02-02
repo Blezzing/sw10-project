1addb37088f739f6c33d3284d11694109d76d548
Cleevil <cleevil@users.noreply.github.com>
2018-02-02 13:03:24 +0100

    Added scribbles of array handling. Also, tree.txt.

--- scribbles/arrays.cpp
+++ scribbles/arrays.cpp
@@ -1,3 +1,22 @@
 int main(){
+	// Create arrays on gpu
 	sgl::GpuArray<int> ar1({1,2,3,4,5});
+	int cpuAr[1024];
+	sgl::GpuArray<int> ar2(cpuAr);
+	sgl::GpuArray<int> ar3(1024);
+
+	//v1, v2, result
+	sgl::add(ar1, ar2, ar3);
+	sgl::substract(ar3, ar2, ar3);
+	
+	ar3 = ar1 + ar2 - ar2
+	ar3 = ar1.add(ar2).substract(ar2);
+	
+	
+	ar1.getArray(); // Transfer array to host
+	ar1.getElement(101); // Transfers single element to host
+	ar1.setArray({1,2,3,4,5}); //overwrites existing gpu array
+	ar1.setElement(1,1); //overwrites a single eleent at index 
 }
+
+zip, map, reduce, filter, 
